aa06055044b85ff74095a92be3729b7edfca3626
Sam-690 <56923470+Sam-690@users.noreply.github.com>
2023-08-26 08:34:42 -0600

    first coomit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Calendar from "./scenes/calendar";
+import Bar from "./scenes/bar"
+import Geography from "./scenes/geography"
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true)
+
+
+  return ( 
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div clasName="app">
+            <Sidebar isSidebar={isSidebar} /> 
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/calendar" element={<Calendar />}/>
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/geography" element={<Geography />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/pie" element={<Pie />} />
+              </Routes>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
